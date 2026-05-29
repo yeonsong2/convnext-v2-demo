706a4d8fb40f19794ebeb26a2726afaece7560f9
yeonsong2 <yeonsong.ing@gmail.com>
2026-05-29 17:00:41 +0900

    add models/__init__.py

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .convnextv2 import (
+    ConvNeXtV2, MODEL_CONFIGS as V2_CONFIGS,
+    convnextv2_atto, convnextv2_femto, convnextv2_pico, convnextv2_nano,
+    convnextv2_tiny, convnextv2_base, convnextv2_large, convnextv2_huge,
+)
+from .convnextv1 import (
+    ConvNeXtV1, MODEL_CONFIGS as V1_CONFIGS,
+    convnext_tiny, convnext_small, convnext_base, convnext_large, convnext_huge,
+)
